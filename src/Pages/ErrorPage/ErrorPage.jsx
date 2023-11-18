a1b524da421df90d53bb3361ff24a7328a26cf51
Orchizakia00 <zakiaislam290@gmail.com>
2023-11-18 14:01:14 +0600

    basic setup of project completed

---
+++ src/Pages/ErrorPage/ErrorPage.jsx
@@ -0,0 +1,10 @@
+
+const ErrorPage = () => {
+    return (
+        <div className="flex my-auto min-h-screen justify-center items-center text-center text-2xl">
+            Page Not Found
+        </div>
+    );
+};
+
+export default ErrorPage;
---
+++ src/Pages/Home/Home/Home.jsx
@@ -0,0 +1,10 @@
+
+const Home = () => {
+    return (
+        <div>
+            home coming soon...
+        </div>
+    );
+};
+
+export default Home;
