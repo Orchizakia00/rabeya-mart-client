8cbe6218994db3c84ec4bbe1f330cc3216f026ee
Tanzim Orchi <zakiaislam290@gmail.com>
2024-09-09 00:07:57 +0600

    products can be inserted into cart

--- src/Components/Shared/ProductCard/ProductCard.jsx
+++ src/Components/Shared/ProductCard/ProductCard.jsx
@@ -1,8 +1,27 @@
+import toast from "react-hot-toast";
 import { FaCartPlus } from "react-icons/fa";
 import { FaBan } from "react-icons/fa";
+import useAxiosPublic from "../../../Hooks/useAxiosPublic";
 
 
 const ProductCard = ({ product }) => {
+    const axiosPublic = useAxiosPublic();
+
+    const handleAddToCart = (product) => {
+        // console.log(product);
+        // toast.success("Product added to cart!");
+        axiosPublic.post('/cart', product)
+            .then(res => {
+                console.log(res.data);
+                if (res.data.insertedId) {
+                    toast.success("Product added to cart!");
+                    // reset();
+                }
+            })
+    
+
+    };
+
     return (
         <div className="card w-96 bg-white shadow-xl">
             <figure className="px-10 pt-10">
@@ -17,7 +36,7 @@ const ProductCard = ({ product }) => {
                         product.availability == false ?
                             <button className="btn border-orange-600 text-orange-600 btn-outline w-full hover:bg-orange-600 hover:border-none hover:text-white cursor-not-allowed"><FaBan /> Out of Stock</button>
                             :
-                            <button className="btn border-orange-600 text-orange-600 btn-outline w-full hover:bg-orange-600 hover:border-none hover:text-white"><FaCartPlus /> Add To Cart</button>
+                            <button onClick={() => handleAddToCart(product)} className="btn border-orange-600 text-orange-600 btn-outline w-full hover:bg-orange-600 hover:border-none hover:text-white"><FaCartPlus /> Add To Cart</button>
                     }
                 </div>
             </div>
