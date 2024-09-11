4953a869eefa47c3e589532eafc3ca6233ccea72
Tanzim Orchi <zakiaislam290@gmail.com>
2024-09-11 22:29:00 +0600

    modified cart page design

--- src/Pages/Cart/Cart.jsx
+++ src/Pages/Cart/Cart.jsx
@@ -24,38 +24,49 @@ const Cart = () => {
         <div className="min-h-screen">
             <SectionTitle heading={'Your Cart'} subHeading={'View Your Cart'}></SectionTitle>
 
-            {
-                cart.map((product) => (
-                    <div key={product._id} className="card card-side bg-base-100 shadow-xl mb-10">
-                        <figure>
-                            <img
-                                className="w-[300px] h-[300px]"
-                                src={product.img}
-                                alt={product.productName} />
-                        </figure>
-                        <div className="card-body">
-                            <h2 className="card-title">{product.productName}</h2>
-                            <p>Price: {product.price}</p>
-                            <div className="card-actions justify-end items-center">
-                                <p className="flex justify-end text-lg">{product.quantity}</p>
-                                <button><FaTrashAlt /></button>
+            <div className="flex justify-between">
+                <div>
+                    {
+                        cart.map((product) => (
+                            <div key={product._id} className="card card-side bg-base-100 shadow-xl mb-10">
+                                <figure>
+                                    <img
+                                        className="w-[400px] h-[300px]"
+                                        src={product.img}
+                                        alt={product.productName} />
+                                </figure>
+                                <div className="card-body">
+                                    <h2 className="card-title">{product.productName}</h2>
+                                    <p>Price: {product.price} TK</p>
+                                    <div className="card-actions justify-end items-center">
+                                        <p className="flex justify-end text-lg">{product.quantity}</p>
+                                        <button><FaTrashAlt /></button>
+                                    </div>
+                                </div>
+                            </div>
+                        ))
+                    }
+                </div>
+                {
+                    cart.length === 0 ? (
+                        <div className="flex justify-center items-center text-xl min-h-[calc(100vh-300px)]">
+                            Your cart is empty
+                        </div>
+                    ) : (
+                        <div className="bg-white py-8 px-6 shadow rounded-lg h-fit">
+                            <p className="text-center text-xl font-semibold mb-6">Order Summary</p>
+                            <p className="flex justify-between gap-20">Subtotal ({cart.length} items): <span>50 BDT</span></p>
+                            <p className="flex justify-between gap-20">Delivery Charge: <span>50 BDT</span></p>
+                            <p className="flex justify-between gap-20 mt-6">Total Price: <span>50 BDT</span></p>
+                            <div className="flex justify-end mt-10">
+                                <button className="btn w-full text-white bg-orange-600 hover:bg-orange-700">Proceed To Confirm</button>
                             </div>
                         </div>
-                    </div>
-                ))
-            }
+                    )
+                }
+            </div>
 
-            {
-                cart.length === 0 ? (
-                    <div className="flex justify-center items-center text-xl min-h-[calc(100vh-300px)]">
-                        Your cart is empty
-                    </div>
-                ) : (
-                    <div className="flex justify-end mt-10">
-                        <button className="btn btn-outline text-white hover:text-orange-600 bg-orange-600 hover:bg-white border-white hover:border-orange-600">Place Order</button>
-                    </div>
-                )
-            }
+
 
 
         </div>
