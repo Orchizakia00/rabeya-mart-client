480c1b0ae49e51e41f81f980197095b51d1ce804
Tanzim Orchi <zakiaislam290@gmail.com>
2024-09-12 14:52:53 +0600

    cart item update in progress

--- src/Components/Shared/ProductCard/ProductCard.jsx
+++ src/Components/Shared/ProductCard/ProductCard.jsx
@@ -3,7 +3,7 @@ import { FaCartPlus } from "react-icons/fa";
 import { FaBan } from "react-icons/fa";
 import useAxiosPublic from "../../../Hooks/useAxiosPublic";
 import useAuth from "../../../Hooks/useAuth";
-// import useCart from "../../../Hooks/useCart";
+import useCart from "../../../Hooks/useCart";
 
 
 const ProductCard = ({ product }) => {
@@ -12,6 +12,7 @@ const ProductCard = ({ product }) => {
     // console.log('point', user);
     const loggedInUser = user?.user;
     const userEmail = loggedInUser?.email;
+    const { data: cart = [] } = useCart();
 
     const handleAddToCart = (product) => {
 
@@ -19,22 +20,44 @@ const ProductCard = ({ product }) => {
             productId: product._id,
             productName: product.productName,
             price: product.price,
-            quantity: product.quantity,
+            quantity: 1,
             img: product.img,
             userEmail
         };
 
-        axios.post('/cart', cartItem)
-            .then(res => {
-                console.log(res.data);
-                if (res.data.insertedId) {
-                    toast.success("Product added to cart!");
-                    // reset();
-                }
-                else {
-                    console.log("Something went wrong! Please try again.");
-                }
-            })
+        const existingCartItem = cart.filter(item => item.productId === product._id);
+        console.log(existingCartItem);
+
+        if (existingCartItem) {
+            // toast.error("Product is already in the cart!"); 
+            const updatedCartItem = {
+                ...existingCartItem,
+                quantity: existingCartItem.quantity + 1  // Increment quantity by 1
+            };
+            axios.put(`/cart/${existingCartItem._id}`, updatedCartItem)
+                .then(res => {
+                    console.log(res.data);
+                    toast.success("Cart updated successfully!");
+                })
+                .catch(err => {
+                    console.error(err);
+                    toast.error("Failed to update cart.");
+                });
+        }
+
+        else {
+            axios.post('/cart', cartItem)
+                .then(res => {
+                    console.log(res.data);
+                    if (res.data.insertedId) {
+                        toast.success("Product added to cart!");
+                        // reset();
+                    }
+                    else {
+                        console.log("Something went wrong! Please try again.");
+                    }
+                })
+        }
     };
 
     return (
