be7f12848586457dc143f6108eef2a8573023ce0
Tanzim Orchi <zakiaislam290@gmail.com>
2024-09-18 00:30:49 +0600

    spinner repostioned

--- src/Routes/PrivateRoute.jsx
+++ src/Routes/PrivateRoute.jsx
@@ -7,7 +7,7 @@ const PrivateRoute = ({ children }) => {
     const { user, loading } = useAuth();
 
     if (loading) {
-        return <span className="loading loading-spinner text-orange-600 loading-lg flex justify-center items-center mx-auto my-10"></span>
+        return <span className="loading loading-spinner min-h-[calc(100vh-150px)] text-orange-600 loading-lg flex justify-center items-center mx-auto my-10"></span>
     }
 
     if (!user) {
