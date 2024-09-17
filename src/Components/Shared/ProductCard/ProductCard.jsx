f080c631e7d12809821579341e08176ffc50b387
Tanzim Orchi <zakiaislam290@gmail.com>
2024-09-17 16:56:22 +0600

    cart bug fixing progressed

--- src/Components/Shared/ProductCard/ProductCard.jsx
+++ src/Components/Shared/ProductCard/ProductCard.jsx
@@ -3,7 +3,7 @@ import { FaCartPlus } from "react-icons/fa";
 import { FaBan } from "react-icons/fa";
 import useAxiosPublic from "../../../Hooks/useAxiosPublic";
 import useAuth from "../../../Hooks/useAuth";
-// import useCart from "../../../Hooks/useCart";
+import useCart from "../../../Hooks/useCart";
 
 
 const ProductCard = ({ product }) => {
@@ -11,52 +11,52 @@ const ProductCard = ({ product }) => {
     const user = useAuth();
     const loggedInUser = user?.user;
     const userEmail = loggedInUser?.email;
-    // const { data: cart = [] } = useCart();
+    const cart = useCart();
 
     const handleAddToCart = (product) => {
-
         const cartItem = {
             productId: product._id,
             productName: product.productName,
             price: product.price,
-            quantity: 1,
+            quantity: product.quantity,
             img: product.img,
             userEmail
         };
-        // console.log('object', cartItem);
+        console.log('cartItem', cartItem);
+        console.log('cart', cart);
 
-        // const existingCartItem = cart.filter(item => item.productId === cartItem.productId);
-        // console.log(existingCartItem);
+        const isItemInCart = cart.some(item => item.productId === cartItem.productId);
+        console.log('isItemInCart', isItemInCart);        
 
-        // if (existingCartItem) {
-        //     // toast.error("Product is already in the cart!"); 
-        //     const updatedCartItem = {
-        //         ...existingCartItem,
-        //         quantity: existingCartItem.quantity + 1  // Increment quantity by 1
-        //     };
-        //     axios.put(`/cart/${cartItem._id}`, updatedCartItem)
-        //         .then(res => {
-        //             console.log(res.data);
-        //             toast.success("Cart updated successfully!");
-        //         })
-        //         .catch(err => {
-        //             console.error(err);
-        //             toast.error("Failed to update cart.");
-        //         });
-        // }
-
+        if (isItemInCart === true) {
+            const updatedCartItem = {
+                ...isItemInCart,
+                quantity: isItemInCart.quantity + 1 // Increment quantity by 1
+            };
+            axios.put(`/cart/${ isItemInCart._id }`, updatedCartItem) // Use existingCartItem's ID
+                .then(res => {
+                    console.log(res.data);
+                    toast.success("Cart updated successfully!");
+                })
+                .catch(err => {
+                    console.error(err);
+                    toast.error("Failed to update cart.");
+                });
+        } else 
         {
             axios.post('/cart', cartItem)
                 .then(res => {
                     console.log(res.data);
                     if (res.data.insertedId) {
                         toast.success("Product added to cart!");
-                        // reset();
-                    }
-                    else {
+                    } else {
                         console.log("Something went wrong! Please try again.");
                     }
                 })
+                .catch(err => {
+                    console.error(err);
+                    toast.error("Failed to add to cart.");
+                });
         }
     };
 
