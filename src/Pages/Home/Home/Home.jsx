ba8fa1433be3a9d0c4e7db786e090fae5ae0842d
Tanzim Orchi <zakiaislam290@gmail.com>
2024-11-19 15:04:53 +0600

    sales section added

--- src/Pages/Home/Home/Home.jsx
+++ src/Pages/Home/Home/Home.jsx
@@ -1,12 +1,14 @@
 import Banner from "../Banner/Banner";
 import Category from "../Category/Category";
 import Popular from "../Popular/Popular";
+import Sales from "../Sales/Sales";
 
 const Home = () => {
     return (
         <div>
             <Banner />
             <Category />
+            <Sales />
             <Popular />
         </div>
     );
---
+++ src/Pages/Home/Sales/Sales.jsx
@@ -0,0 +1,26 @@
+import sales from '../../../assets/sales/sales.jpg'
+
+const Sales = () => {
+    return (
+        <div
+            className="min-h-screen flex items-center justify-end my-14"
+            style={{
+                backgroundImage: `url(${sales})`,
+                backgroundSize: '110%', // Zoom out effect by increasing the size
+                backgroundPosition: 'center', // Keep the image centered
+                backgroundRepeat: 'no-repeat', // Prevent tiling
+            }}>
+            <div className="bg-opacity-20"></div>
+            <div className="text-black text-right">
+                <div className="mr-10">
+                    <h1 className="mb-10 text-4xl font-bold">Exclusive Offers</h1>
+                    <p className="mb-5 text-2xl max-w-lg space-y-4">
+                        Enjoy up to <span className='text-orange-600 font-bold'>30%</span> off on fresh fruits and vegetables. Stock up on healthy essentials and save more today!
+                    </p>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Sales;
